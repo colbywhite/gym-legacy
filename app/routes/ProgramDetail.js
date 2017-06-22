541a21b169c3e0716924c3fece3df58cb7c9dc6f
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-22 16:51:35 -0500

    temp layout for program detail

--- app/routes/ProgramDetail.js
+++ app/routes/ProgramDetail.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
+// TODO: * calculate a schedule with rest days included
+// * then display them in seven-day increments (swipe to go between the weeks)
+// * then make each day expandable to see the workout for that day
+class SingleDay extends Component {
+  render() {
+    const workout = this.props.workout
+    return (
+      <ListItem
+        title={workout.name}
+        hideChevron={true}
+        />
+    )
+  }
+}
 
 export default class ProgramDetail extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -8,9 +23,11 @@ export default class ProgramDetail extends Component {
   render() {
     const { program } = this.props.navigation.state.params;
     return (
-      <Text>
-       {program.name} - {program.schedule.type}
-      </Text>
+      <List>
+       {program.workouts.map((workout, i) => (
+         <SingleDay workout={workout} key={i}/>
+       ))}
+      </List>
     )
   }
 }
