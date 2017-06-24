23d283dac9125ca1017dbd5c6b4d7820bd748d46
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-23 21:50:13 -0500

    calculate weekly schedule

--- app/routes/ProgramDetail.js
+++ app/routes/ProgramDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { List, ListItem } from 'react-native-elements'
 import SwipeALot from 'react-native-swipe-a-lot'
+import { schedule_calculator } from 'weight-program-schema'
 
 // TODO: * calculate a schedule with rest days included
 // * then make each day expandable to see the workout for that day
@@ -9,9 +10,9 @@ class WeekSchedule extends Component {
     const workouts = this.props.workouts
     return (
       <List>
-        {workouts.map((workout, i) => {
+        {workouts.map((day, i) => {
           return (<ListItem
-            title={workout.name}
+            title={day === 'rest' ? day : 'lift'}
             key={i}
             hideChevron={true}
             />
@@ -22,17 +23,32 @@ class WeekSchedule extends Component {
   }
 }
 
+const spliceIntoChunks = (arr, chunkLength) => {
+  var results = [],
+    numChunks = Math.ceil(arr.length / chunkLength)
+    i = 0,
+    j = chunkLength - 1;
+  while(results.length < numChunks) {
+    results.push(arr.slice(i, j+1))
+    i += chunkLength
+    j += chunkLength
+  }
+  return results;
+}
+
 export default class ProgramDetail extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: `${navigation.state.params.program.name} Detail`
   })
   render() {
-    const { program } = this.props.navigation.state.params;
+    const { program } = this.props.navigation.state.params,
+      sked = schedule_calculator(program),
+      weeks = spliceIntoChunks(sked, 7)
     return (
       <SwipeALot>
-        <WeekSchedule workouts={program.workouts} />
-        <WeekSchedule workouts={program.workouts} />
-        <WeekSchedule workouts={program.workouts} />
+        {weeks.map((week, i) => {
+          return (<WeekSchedule key={i} workouts={week} />)
+        })}
       </SwipeALot>
     )
   }
