65982d0e6c8d5e733a9976d1faa2e0c1d9be00e6
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-26 22:10:39 -0500

    Revert "expandable cards for a week's schedule"
    This reverts commit 96f8f721f61e54daa17e64623e2f0ccd07a51faa.
    
    nativebase breaks jest, meaning the tests are broken. moving away
    from nativebase :cry:

--- app/components/WeekSchedule.js
+++ app/components/WeekSchedule.js
@@ -1,53 +1,22 @@
 import React, { Component } from 'react';
-import { Container, Content, Card, CardItem, Body, Text } from 'native-base';
+import { List, ListItem } from 'react-native-elements'
 
+// TODO: * calculate a schedule with rest days included
+// * then make each day expandable to see the workout for that day
 export default class WeekSchedule extends Component {
-  constructor(props) {
-    super(props)
-    const workouts = this.props.workouts
-    this.state = {dayExpanded: workouts.map((w) => { return false})}
-  }
-
-  _onPress(e) {
-    console.log('press', e)
-    this.setState((previous) => {
-      previous.dayExpanded[e] = !previous.dayExpanded[e]
-      return previous
-    })
-  }
-
   render() {
     const workouts = this.props.workouts
-
     return (
-      <Container>
-        <Content>
-          {workouts.map((workout, i) => {
-            const isExpanded = this.state.dayExpanded[i]
-            return (
-              <Card key={i}>
-                <CardItem button={true} onPress={this._onPress.bind(this, i)}>
-                  <Body>
-                      <Text>
-                        {(workout === 'rest') ? workout : 'lift'}
-                      </Text>
-                  </Body>
-                </CardItem>
-                {(isExpanded) ?
-                  <CardItem>
-                    <Body>
-                        <Text>
-                          CONTENT
-                        </Text>
-                    </Body>
-                  </CardItem>
-                  : null
-                }
-              </Card>
-            )
-          })}
-        </Content>
-      </Container>
+      <List>
+        {workouts.map((day, i) => {
+          return (<ListItem
+            title={day === 'rest' ? day : 'lift'}
+            key={i}
+            hideChevron={true}
+            />
+          )
+        })}
+      </List>
     )
   }
 }
