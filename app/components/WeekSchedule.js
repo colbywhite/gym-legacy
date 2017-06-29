8f648ea950ef3b82306c7111db091c3b43f5d82a
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-28 19:35:50 -0500

    add lift day component

--- app/components/WeekSchedule.js
+++ app/components/WeekSchedule.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { List, ListItem } from 'react-native-elements'
 import RestDay from './RestDay'
+import ExpandableLiftDay from './ExpandableLiftDay'
 
 // TODO: * calculate a schedule with rest days included
 // * then make each day expandable to see the workout for that day
@@ -27,22 +28,10 @@ export default class WeekSchedule extends Component {
             return ( <RestDay key={i} /> )
           }
           return (
-            <View key={i}>
-              <ListItem
-                key={i}
-                title={'lift'}
-                leftIcon={{name:'fitness-center'}}
-                rightIcon={{name:'expand-more'}}
-                onPress={this._toggleDaysExpansion.bind(this, i)}
-                />
-              {expansionStates[i] &&
-                <ListItem
-                  title={`Day ${i+1}`}
-                  hideChevron={true}
-                />
-              }
-            </View>
-          )
+            <ExpandableLiftDay key={i}
+              expanded={expansionStates[i]}
+              onPress={this._toggleDaysExpansion.bind(this, i)}
+              /> )
         })}
       </List>
     )
