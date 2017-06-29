dd28f433d8c46f96ea8a2bafbb237107aa38517c
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-28 19:29:01 -0500

    add rest day component

--- app/components/WeekSchedule.js
+++ app/components/WeekSchedule.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { List, ListItem } from 'react-native-elements'
+import RestDay from './RestDay'
 
 // TODO: * calculate a schedule with rest days included
 // * then make each day expandable to see the workout for that day
@@ -22,17 +23,19 @@ export default class WeekSchedule extends Component {
     return (
       <List>
         {workouts.map((day, i) => {
-          const isRestDay = day === 'rest'
+          if (day === 'rest') {
+            return ( <RestDay key={i} /> )
+          }
           return (
             <View key={i}>
               <ListItem
-                title={isRestDay ? day : 'lift'}
-                leftIcon={{name:isRestDay ? 'battery-charging-full' : 'fitness-center'}}
+                key={i}
+                title={'lift'}
+                leftIcon={{name:'fitness-center'}}
                 rightIcon={{name:'expand-more'}}
-                hideChevron={isRestDay}
                 onPress={this._toggleDaysExpansion.bind(this, i)}
                 />
-              {!isRestDay && expansionStates[i] &&
+              {expansionStates[i] &&
                 <ListItem
                   title={`Day ${i+1}`}
                   hideChevron={true}
