5d1f5429841f1dd73c60575286edd5be8b51d94b
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-29 19:53:42 -0500

    pivoting to web app, not mobile app
    there's no need for this to be a mobile app other than the fact
    that most apps that do this sort of thing are mobile apps. but the
    problem that's being solved doesn't require it to be a mobile app.
    this can be done via a mobile-friendly web app. and since i plan
    very little frills in terms of design, a web app should be perfectly
    sufficient

--- app/config/routes.js
+++
@@ -1,9 +0,0 @@
-import ProgramList from '../routes/ProgramList'
-import ProgramDetail from '../routes/ProgramDetail'
-
-const routes = {
-  ProgramDetail: {screen: ProgramDetail},
-  ProgramList: { screen: ProgramList }
-}
-
-export default routes
