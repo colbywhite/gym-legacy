380c4c5c903d0c37c845b996bd01eb252b1899be
Colby M. White <colbywhite@users.noreply.github.com>
2017-06-30 14:42:41 -0500

    add default programs

--- src/app/program-list.component.ts
+++ src/app/program-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// TODO I'd much rather to import { programs } from 'weight-program-schema'
+import * as stronglifts from 'weight-program-schema/lib/stronglifts.json'
+import * as candito_squat from 'weight-program-schema/lib/candito_squat.json'
 
 @Component({
   selector: 'gl-program-list',
@@ -6,11 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./program-list.component.css']
 })
 export class ProgramListComponent {
-  public programs: Program[] = [
-    {name: 'hello'},  {name: 'world!'}
-  ]
-}
-
-interface Program {
-  name: string
+  public programs: any[] = [stronglifts, candito_squat]
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,9 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+// TODO this shouldn't really be needed to require 'weight-program-schema'
+declare module '*.json' {
+    const value: any;
+    export default value;
+}
