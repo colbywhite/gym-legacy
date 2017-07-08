cf95eb11a4387c24837727357e7175ea33214ef2
Colby M. White <colbywhite@users.noreply.github.com>
2017-07-08 03:04:37 -0600

    move json typings

--- src/app/shared/program.service.ts
+++ src/app/shared/program.service.ts
@@ -1,7 +1,10 @@
+///<reference path="../typings/json-typings.d.ts" />
+
 import { Injectable } from '@angular/core';
 
 import * as stronglifts from 'weight-program-schema/lib/stronglifts.json'
 import * as candito_squat from 'weight-program-schema/lib/candito_squat.json'
+
 @Injectable()
 export class ProgramService {
 
