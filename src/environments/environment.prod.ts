9cbd339cc961c5906c550df9db31217a2a755f28
Colby M. White <colbywhite@users.noreply.github.com>
2017-07-30 21:55:26 -0500

    add dev apiurl to prod for now

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
+// TODO: don't use the dev apiUrl
 export const environment = {
   auth: {
     clientID: '%CLIENTID%',
     redirectUri: 'http://d1g8look55h14i.cloudfront.net/callback'
   },
+  apiUrl: 'https://keub21khij.execute-api.us-east-1.amazonaws.com/dev',
   production: true
 };
