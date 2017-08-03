9c6261711714d2319885d3d185e7326f055fa161
Colby M. White <colbywhite@users.noreply.github.com>
2017-08-02 22:16:38 -0500

    change frontend to point to new endpoints

--- backend/activeProgram.js
+++ backend/activeProgram.js
@@ -2,21 +2,26 @@ const AWS = require('aws-sdk');
 const BbPromise = require('bluebird');
 const client = BbPromise.promisifyAll(new AWS.DynamoDB.DocumentClient());
 
+const HEADERS = {
+  "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+  "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+}
+
 const sendResponse = (status, callback, body) => {
   const response = {
+    headers: HEADERS,
     statusCode: status,
     body: JSON.stringify(body)
   }
   callback(null, response)
 }
 
-const createActiveProgram = (userid, start_date, name, program) => {
+const createActiveProgram = (userid, start_date, program) => {
   const item = {
     user_id: userid,
-    name: name,
-    start_date: start_date.toISOString(),
-    program: program
+    start_date: start_date.toISOString()
   }
+  Object.assign(item, program)
   const params = {
     TableName: 'active_programs',
     Item: item
@@ -42,7 +47,7 @@ module.exports.create = (event, context, callback) => {
   const errorResponse = sendResponse.bind(null, 500, callback)
   const user_id = event.requestContext.authorizer.principalId
   const body = JSON.parse(event.body)
-  createActiveProgram(user_id, new Date(), body.name, body.program)
+  createActiveProgram(user_id, new Date(), body)
     .then(successResponse)
     .catch(errorResponse)
 }
@@ -51,7 +56,7 @@ module.exports.delete = (event, context, callback) => {
   const successResponse = sendResponse.bind(null, 200, callback)
   const errorResponse = sendResponse.bind(null, 500, callback)
   const user_id = event.requestContext.authorizer.principalId
-  const name = event.pathParameters.name
+  const name = decodeURIComponent(event.pathParameters.name)
   // TODO return No Content
   deleteActiveProgram(user_id, name)
     .then(successResponse)
