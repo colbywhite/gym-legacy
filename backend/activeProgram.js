5b88f8d769ae695fd86536f02ec1048cf3e66a62
Colby M. White <colbywhite@users.noreply.github.com>
2017-08-02 21:30:12 -0500

    adding delete activeProgram endpoint

--- backend/activeProgram.js
+++ backend/activeProgram.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 const BbPromise = require('bluebird');
+const client = BbPromise.promisifyAll(new AWS.DynamoDB.DocumentClient());
 
 const sendResponse = (status, callback, body) => {
   const response = {
@@ -10,7 +11,6 @@ const sendResponse = (status, callback, body) => {
 }
 
 const createActiveProgram = (userid, start_date, name, program) => {
-  const client = BbPromise.promisifyAll(new AWS.DynamoDB.DocumentClient());
   const item = {
     user_id: userid,
     name: name,
@@ -21,15 +21,39 @@ const createActiveProgram = (userid, start_date, name, program) => {
     TableName: 'active_programs',
     Item: item
   }
+  console.log('Creating', item)
+  // TODO should I check if it already exists?
   return client.putAsync(params).then(() => item)
 }
 
+const deleteActiveProgram = (userid, name) => {
+  const params = {
+    TableName: 'active_programs',
+    Key: {
+      user_id: userid,
+      name: name
+    }
+  }
+  console.log('Deleting', params.Key)
+  return client.deleteAsync(params)
+}
 module.exports.create = (event, context, callback) => {
   const successResponse = sendResponse.bind(null, 200, callback)
   const errorResponse = sendResponse.bind(null, 500, callback)
   const user_id = event.requestContext.authorizer.principalId
   const body = JSON.parse(event.body)
   createActiveProgram(user_id, new Date(), body.name, body.program)
+    .then(successResponse)
+    .catch(errorResponse)
+}
+
+module.exports.delete = (event, context, callback) => {
+  const successResponse = sendResponse.bind(null, 200, callback)
+  const errorResponse = sendResponse.bind(null, 500, callback)
+  const user_id = event.requestContext.authorizer.principalId
+  const name = event.pathParameters.name
+  // TODO return No Content
+  deleteActiveProgram(user_id, name)
     .then(successResponse)
     .catch(errorResponse)
 }
